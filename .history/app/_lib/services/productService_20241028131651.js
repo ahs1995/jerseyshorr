b90a24c16196afcaf76a4a9504580b9c47c46597
ahs.1995 <ashik.srkr95@gmail.com>
2024-10-28 13:45:41 +0530

    fix: Include renamed images in public folder
    ensures all image files in the public folder have lowercase names to resolve loading issue in production. this commit corrects missed file changes from the previous commit.

---
+++ .history/app/_lib/services/productService_20241028131651.js
@@ -0,0 +1,117 @@
+import { dbConnect } from "@/utils/mongodb";
+import Product from "@/models/Product";
+
+export async function getProducts() {
+  try {
+    await dbConnect();
+
+    const products = await Product.find({}).lean();
+
+    // Transform products to plain JS objects
+    const transformedProducts = products.map((product) => ({
+      ...product,
+      _id: product._id.toString(), // Convert _id to string
+      // createdAt: product.createdAt ? product.createdAt.toISOString() : null, // Convert createdAt to ISO string
+    }));
+
+    // Group products by style
+
+    const groupedProducts = {
+      products: transformedProducts,
+      byStyle: {},
+      newArrivals: [],
+      teams: {},
+    };
+
+    const teamSet = new Set();
+
+    transformedProducts.forEach((product) => {
+      // Group by style
+      if (!groupedProducts.byStyle[product.style]) {
+        groupedProducts.byStyle[product.style] = [];
+      }
+      groupedProducts.byStyle[product.style].push(product);
+
+      // Track unique team
+      teamSet.add(product.team);
+
+      //Check if product is a new arrival
+      const createdAt = new Date(product.createdAt);
+      const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+      if (createdAt > sevenDaysAgo) {
+        groupedProducts.newArrivals.push(product);
+      }
+    });
+
+    // Create team data structure
+    groupedProducts.teams = Array.from(teamSet).map((team) => ({
+      name: team,
+      imageUrl: `/team-logos/${team}.png`,
+      productsCount: transformedProducts.filter((p) => p.team === team).length,
+    }));
+
+    return groupedProducts;
+  } catch (e) {
+    console.error(e);
+    throw new Error("Failed to fetch product");
+  }
+}
+
+// import clientPromise from "@/utils/mongodb";
+// import { ObjectId } from "mongodb";
+
+// // Get all products documents
+// export async function getProducts() {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("jerseyshorr");
+
+//     const products = await db.collection("products").find({}).toArray();
+
+//     // Group products by style
+//     const groupedProducts = {
+//       products,
+//       byStyle: {},
+//       newArrivals: [],
+//       teams: {},
+//     };
+
+//     const teamSet = new Set();
+
+//     products.forEach((product) => {
+//       // Convert _id and createdAt to plain strings
+//       product._id = product._id.toString();
+//       product.createdAt =
+//         product.createdAt instanceof Date
+//           ? product.createdAt.toISOString()
+//           : product.createdAt;
+//       // Group by style
+//       if (!groupedProducts.byStyle[product.style]) {
+//         groupedProducts.byStyle[product.style] = [];
+//       }
+//       groupedProducts.byStyle[product.style].push(product);
+
+//       // Track unique team
+//       teamSet.add(product.team);
+
+//       //Check if product is a new arrival
+//       const createdAt = new Date(product.createdAt);
+//       const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+//       if (createdAt > sevenDaysAgo) {
+//         groupedProducts.newArrivals.push(product);
+//       }
+//     });
+
+//     // Create team data structure
+//     groupedProducts.teams = Array.from(teamSet).map((team) => ({
+//       name: team,
+//       imageUrl: `/team-logos/${team.toLowerCase()}.png`,
+//       productsCount: products.filter((p) => p.team === team).length,
+//     }));
+
+//     return groupedProducts;
+//   } catch (e) {
+//     console.error(e);
+//     throw new Error("Failed to fetch products");
+//   }
+// }
---
+++ .history/app/_lib/services/productService_20241028131733.js
@@ -0,0 +1,58 @@
+import { dbConnect } from "@/utils/mongodb";
+import Product from "@/models/Product";
+
+export async function getProducts() {
+  try {
+    await dbConnect();
+
+    const products = await Product.find({}).lean();
+
+    // Transform products to plain JS objects
+    const transformedProducts = products.map((product) => ({
+      ...product,
+      _id: product._id.toString(), // Convert _id to string
+      // createdAt: product.createdAt ? product.createdAt.toISOString() : null, // Convert createdAt to ISO string
+    }));
+
+    // Group products by style
+
+    const groupedProducts = {
+      products: transformedProducts,
+      byStyle: {},
+      newArrivals: [],
+      teams: {},
+    };
+
+    const teamSet = new Set();
+
+    transformedProducts.forEach((product) => {
+      // Group by style
+      if (!groupedProducts.byStyle[product.style]) {
+        groupedProducts.byStyle[product.style] = [];
+      }
+      groupedProducts.byStyle[product.style].push(product);
+
+      // Track unique team
+      teamSet.add(product.team);
+
+      //Check if product is a new arrival
+      const createdAt = new Date(product.createdAt);
+      const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+      if (createdAt > sevenDaysAgo) {
+        groupedProducts.newArrivals.push(product);
+      }
+    });
+
+    // Create team data structure
+    groupedProducts.teams = Array.from(teamSet).map((team) => ({
+      name: team,
+      imageUrl: `/team-logos/${team}.png`,
+      productsCount: transformedProducts.filter((p) => p.team === team).length,
+    }));
+
+    return groupedProducts;
+  } catch (e) {
+    console.error(e);
+    throw new Error("Failed to fetch product");
+  }
+}

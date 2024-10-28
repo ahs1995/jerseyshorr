e510bf352d42f68ea29023aceec9727d1e08502d
ahs.1995 <ashik.srkr95@gmail.com>
2024-10-28 13:40:48 +0530

    fix: standardize image names to lowercase for production
    what:
    renamed all the image files in the public folder to lowercase to resolve inconsistencies in production.
    
    why:
    production servers are case sensitive, causing issues where some images with uppercase names were not loading correctly.
    
    impact:
    ensures all images load consistently accross environments without the need for `.toLowerCase()` during URL generation.

--- app/_lib/services/productService.js
+++ app/_lib/services/productService.js
@@ -46,7 +46,7 @@ export async function getProducts() {
     // Create team data structure
     groupedProducts.teams = Array.from(teamSet).map((team) => ({
       name: team,
-      imageUrl: `/team-logos/${team.toLowerCase()}.png`,
+      imageUrl: `/team-logos/${team}.png`,
       productsCount: transformedProducts.filter((p) => p.team === team).length,
     }));
 
@@ -56,62 +56,3 @@ export async function getProducts() {
     throw new Error("Failed to fetch product");
   }
 }
-
-// import clientPromise from "@/utils/mongodb";
-// import { ObjectId } from "mongodb";
-
-// // Get all products documents
-// export async function getProducts() {
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db("jerseyshorr");
-
-//     const products = await db.collection("products").find({}).toArray();
-
-//     // Group products by style
-//     const groupedProducts = {
-//       products,
-//       byStyle: {},
-//       newArrivals: [],
-//       teams: {},
-//     };
-
-//     const teamSet = new Set();
-
-//     products.forEach((product) => {
-//       // Convert _id and createdAt to plain strings
-//       product._id = product._id.toString();
-//       product.createdAt =
-//         product.createdAt instanceof Date
-//           ? product.createdAt.toISOString()
-//           : product.createdAt;
-//       // Group by style
-//       if (!groupedProducts.byStyle[product.style]) {
-//         groupedProducts.byStyle[product.style] = [];
-//       }
-//       groupedProducts.byStyle[product.style].push(product);
-
-//       // Track unique team
-//       teamSet.add(product.team);
-
-//       //Check if product is a new arrival
-//       const createdAt = new Date(product.createdAt);
-//       const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
-//       if (createdAt > sevenDaysAgo) {
-//         groupedProducts.newArrivals.push(product);
-//       }
-//     });
-
-//     // Create team data structure
-//     groupedProducts.teams = Array.from(teamSet).map((team) => ({
-//       name: team,
-//       imageUrl: `/team-logos/${team.toLowerCase()}.png`,
-//       productsCount: products.filter((p) => p.team === team).length,
-//     }));
-
-//     return groupedProducts;
-//   } catch (e) {
-//     console.error(e);
-//     throw new Error("Failed to fetch products");
-//   }
-// }
